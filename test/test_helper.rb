cc59c4baad500a29cdc97b3208d569255c25cc88
Yuya Kozuma <hello.potechi@gmail.com>
2023-04-08 14:57:12 +0900

    Setup Test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,22 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+# gem minitest-reporters setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  # 並列テスト とは複数のプロセスを分岐させテスト時間の短縮を行う機能
+  #
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    # seedデータの読み込み
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  # 並列テストの有効・無効化
+  # workers: プロセス数を渡す(2以上 => 有効、2未満 => 無効)
+  #number_of_processors => 使用しているマシンのコア数（今回の場合はDockerで4）
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
