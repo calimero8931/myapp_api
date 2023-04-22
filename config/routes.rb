9ad0f9f1928552cb716b95981f829e91216a1f72
Yuya Kozuma <hello.potechi@gmail.com>
2023-04-22 16:24:16 +0900

    add_users_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :hello, only:[:index]
+      # users_controller.rbのindexアクションを呼び出す
+      resources :users, only:[:index]
     end
   end
 end
