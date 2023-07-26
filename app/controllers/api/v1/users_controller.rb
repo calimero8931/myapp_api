bff7882e0b600591b1d16745e712f798d04980b3
Yuya Kozuma <hello.potechi@gmail.com>
2023-07-27 00:16:25 +0900

    Add authenticate service module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  # 認可を行う
+  before_action :authenticate_user
+
   def index
-    users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    # users = User.all
+    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   end
 end
