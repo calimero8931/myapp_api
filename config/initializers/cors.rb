1a3d496a6b0739afe9fa4999857eee3e9fc67bb8
Yuya Kozuma <hello.potechi@gmail.com>
2023-07-29 16:28:55 +0900

    Setup credentials for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # cookieを使うために必要
   end
 end
