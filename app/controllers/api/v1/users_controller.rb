bbd98526d0c9cbbd2e44c9df76659dddbbc0c68a
Yuya Kozuma <hello.potechi@gmail.com>
2023-07-29 12:59:20 +0900

    Add auth token controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,3 @@
 class Api::V1::UsersController < ApplicationController
-  # 認可を行う
-  before_action :authenticate_user
 
-  def index
-    # users = User.all
-    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
-  end
 end
