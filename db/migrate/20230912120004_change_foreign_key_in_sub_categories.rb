61d7884507625fe495c11af5cb4cf76e01ae71ae
Yuya Kozuma <hello.potechi@gmail.com>
2023-10-26 20:15:19 +0900

    adjust migrate heroku3

---
+++ db/migrate/20230912120004_change_foreign_key_in_sub_categories.rb
@@ -0,0 +1,6 @@
+class ChangeForeignKeyInSubCategories < ActiveRecord::Migration[6.1]
+  def change
+    remove_foreign_key :trophies, :categories, column: :category_id
+    add_foreign_key :trophies, :sub_categories, column: :category_id
+  end
+end
